272137510a5ea0b69645802086e4c7cb9c4ac5f1
BCoop123 <brandoncooper859@gmail.com>
2023-03-07 18:03:11 -0500

    added buttons
    +added create post button and logic
    +added delete post button and logic

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,55 @@
+function showCreatePostForm() {
+    // Hide the original "Create New Post" button
+    const createPostButton = document.getElementById("create-post-button");
+    createPostButton.style.display = "none";
+
+// Hide the original "Delete Existing Post" button
+    const deletePostButton = document.getElementById("delete-post-button");
+    deletePostButton.style.display = "none";
+
+// Show the form
+    const createPostForm = document.getElementById("postForm");
+    createPostForm.style.display = "block";
+}
+
+function showDeletePostForm(postId) {
+    // Hide the original "Create New Post" button
+    const createPostButton = document.getElementById("create-post-button");
+    createPostButton.style.display = "none";
+
+    // Hide the original "Delete Existing Post" button
+    const deletePostButton = document.getElementById("delete-post-button");
+    deletePostButton.style.display = "none";
+
+    // Show the form
+    const deletePostForm = document.getElementById(`postForm`);
+    deletePostForm.style.display = "block";
+}
+
+function submitForm() {
+    // Your code to handle form submission goes here
+    console.log('Form submitted!');
+    document.getElementById('create-post-button').style.display = 'block';
+    document.getElementById('delete-post-button').style.display = 'block';
+    document.getElementById('postForm').style.display = 'none';
+}
+
+function cancelForm() {
+    document.getElementById('create-post-button').style.display = 'block';
+    document.getElementById('delete-post-button').style.display = 'block';
+    document.getElementById('postForm').style.display = 'none';
+}
+
+function createPost() {
+    // Your code to create a new post goes here
+    console.log('New post created!');
+}
+
+function deletePost() {
+    // Your code to delete an existing post goes here
+    console.log('Existing post deleted!');
+}  
+
 api.GET('1082768886235152384', function(response) {  
     for (i = 0; i < response.data.length; i++) {
         //template for creating a new post
