6eb556d19c54c9b49a11459271b6df71be292dea
Brooks Arthur <brooksarthur07@gmail.com>
2023-04-05 15:30:59 -0400

    loging routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,16 +5,19 @@ const path = require('path');
 
 // Serve index.html
 router.get('/', (req, res) => {
+  console.log("<INDEX PAGE>");
   res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 // Serve post.html
 router.get('/post', (req, res) => {
+  console.log("<USER POST PAGE>");
   res.sendFile(path.join(__dirname, '../views', 'post.html'));
 })
 
 // Serve user.html
 router.get('/user', (req,res) => {
+  console.log("<CREATE EDIT USER PAGE>");
   res.sendFile(path.join(__dirname, '../views', 'user.html'));
 })
 
