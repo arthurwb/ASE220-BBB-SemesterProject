fbf22ea51868ad2f918b06616dfc600a5159a296
Brooks Arthur <brooksarthur07@gmail.com>
2023-04-10 15:46:39 -0400

    integrated api into node server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,22 +2,22 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
+const api = require('../api/apiHandler');
 
-router.get('/api', (req, res) => {
-    let params = req.params;
-    res.send("api data");
-  });
-  router.post('/api', (req, res) => {
-    let params = req.params;
-    res.send("api data");
-  });
-  router.put('/api', (req, res) => {
-    let params = req.params;
-    res.send("api data");
-  });
-  router.delete('/api', (req, res) => {
-    let params = req.params;
-    res.send("api data");
-  });
+router.get('/api/:param', (req, res) => {
+  let getResponse = api.httpRequest.GET(req.params["param"]);
+  res.writeHead(200,{'Content-Type':'application/json'});
+  res.write(getResponse, 'utf8');
+  res.end();
+}).post('/api', (req, res) => {
+  let params = req.params;
+  res.send("api data");
+}).put('/api', (req, res) => {
+  let params = req.params;
+  res.send("api data");
+}).delete('/api', (req, res) => {
+  let params = req.params;
+  res.send("api data");
+});
 
-  module.exports = apiRouter;
+module.exports = router;
